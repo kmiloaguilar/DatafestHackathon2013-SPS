47fa90cadc9ebe0c79ea4c2727d13b942ae789f2
Camilo Aguilar <camilo@acklenavenue.com>
2013-11-01 09:10:29 -0600

    Blank project for ios versiong. Testflight sdk and parse sdk added

---
+++ ios/DameChamba/DameChamba/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DameChamba
+//
+//  Created by Camilo Aguilar on 11/1/13.
+//  Copyright (c) 2013 Angel Camilo Aguilar Fernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ios/DameChamba/DameChambaTests/DameChambaTests.m
@@ -0,0 +1,34 @@
+//
+//  DameChambaTests.m
+//  DameChambaTests
+//
+//  Created by Camilo Aguilar on 11/1/13.
+//  Copyright (c) 2013 Angel Camilo Aguilar Fernandez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DameChambaTests : XCTestCase
+
+@end
+
+@implementation DameChambaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
